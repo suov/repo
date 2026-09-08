e4535baa3adfa71648249fc942b2df053f502645
suov <suorva27@hotmail.com>
2026-09-08 18:09:28 -0500

    Primer commit

---
+++ src/main/java/com/example/tutoria1/Tutoria1Application.java
@@ -0,0 +1,13 @@
+package com.example.tutoria1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutoria1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutoria1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tutoria1/Tutoria1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutoria1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutoria1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
