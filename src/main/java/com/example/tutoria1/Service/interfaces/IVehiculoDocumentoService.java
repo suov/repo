11e83a3fa89b1b37fe0c661834bb5edb58a9856b
RojasJhonatan <jmr.85@hotmail.com>
2026-09-14 18:12:54 -0500

    controller VehiculoDocumento + corrección error JSON

--- src/main/java/com/example/tutoria1/Service/interfaces/IVehiculoDocumentoService.java
+++ src/main/java/com/example/tutoria1/Service/interfaces/IVehiculoDocumentoService.java
@@ -3,11 +3,8 @@ package com.example.tutoria1.Service.interfaces;
 import java.util.List;
 
 import com.example.tutoria1.Model.VehiculoDocumentoModel;
-import com.example.tutoria1.Model.VehiculoModel;
 
 public interface IVehiculoDocumentoService {
-
-} IVehiculoDocumentoService {
 
     VehiculoDocumentoModel crearVehiculoDocumento(VehiculoDocumentoModel vehiculoDocumento);
 
