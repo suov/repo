c0ef5070253bc5f88368de81c735872031b3d510
RojasJhonatan <jmr.85@hotmail.com>
2026-09-14 11:12:27 -0500

    ajuste clase documento

--- src/main/java/com/example/tutoria1/Model/DocumentoModel.java
+++ src/main/java/com/example/tutoria1/Model/DocumentoModel.java
@@ -3,43 +3,56 @@ package com.example.tutoria1.Model;
 import com.example.tutoria1.Enums.Documento.RequisitoSegunVehiculo;
 import com.example.tutoria1.Enums.Documento.TipoVehiculo;
 
+import jakarta.persistence.CheckConstraint;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter 
-@Setter 
-@Entity 
-@Table (name = "documentos")
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "documentos", check = {
+        /* Condiciones de los atributos de documento */
+        @CheckConstraint(name = "chk_tipo_vehiculo_al_que_aplica", constraint = "tipo_vehiculo_al_que_aplica IN IN ('A', 'M', 'AM')"),
+        @CheckConstraint(name = "chk_requisito_segun_tipo_vehiculo", constraint = "requisito_segun_tipo_vehiculo IN IN ('RA', 'RM', 'RR')")
+})
 public class DocumentoModel {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column (name = "codigo_documento_parametrizado", nullable = false)
+    @Column(name = "codigo_documento_parametrizado", nullable = false)
     private String codigoDocumentoParametrizado;
 
-    @Column (name = "nombre_documento", nullable = false)
+    @Column(name = "nombre_documento", nullable = false)
     private String nombreDocumento;
 
-    /* A - automovil
+    /*
+     * A - automovil
      * M - motocicleta
-     * AM - ambos */
-    @Column (name = "tipo_vehiculo_al_que_aplica", nullable = false)
+     * AM - ambos
+     */
+    @Column(name = "tipo_vehiculo_al_que_aplica", nullable = false)
     private TipoVehiculo tipoVehiculoAlQueAplica;
 
-    /* RA - obligatorio para automovil
+    /*
+     * RA - obligatorio para automovil
      * RM - obligatorio para motocicleta
-     * RR - obligatorio para ambos */
-    @Column (name = "requisito_segun_tipo_vehiculo", nullable = false)
+     * RR - obligatorio para ambos
+     */
+    @Column(name = "requisito_segun_tipo_vehiculo", nullable = false)
     private RequisitoSegunVehiculo requisitoSegunTipoVehiculo;
 
-    @Column (name = "descripcion", nullable = false)
+    @Column(name = "descripcion", nullable = false)
     private String descripcion;
 }
--- src/main/java/com/example/tutoria1/Model/VehiculoModel.java
+++ src/main/java/com/example/tutoria1/Model/VehiculoModel.java
@@ -1,7 +1,5 @@
 package com.example.tutoria1.Model;
 
-import org.hibernate.annotations.Check;
-
 import com.example.tutoria1.Enums.Vehiculo.TipoCombustible;
 import com.example.tutoria1.Enums.Vehiculo.TipoServicio;
 import com.example.tutoria1.Enums.Vehiculo.TipoVehiculo;
@@ -13,7 +11,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.EnumType;
@@ -21,48 +21,50 @@ import jakarta.persistence.EnumType;
 @Entity
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "vehiculos", check = {
-        /* Condiciones de los atributos de vehiculo */
-        @CheckConstraint(name = "chk_tipo_vehiculo", constraint = "tipo_vehiculo IN ('AUTOMOVIL', 'MOTO')"),
-        @CheckConstraint(name = "chk_placa", constraint = "(tipo_vehiculo = 'AUTOMOVIL' AND placa REGEXP '^[A-Za-z]{3}[0-9]{3}$')"
-                +
-                "OR (tipo_vehiculo = 'MOTO' AND placa REGEXP '^[A-Za-z]{3}[0-9]{2}[A-Za-z]$'))"),
-        @CheckConstraint(name = "chk_cap_pasajeros", constraint = "(capacidad_pasajeros >= 0)"),
-        @CheckConstraint(name = "chk_modelo", constraint = "(modelo >= 0)")
+                /* Condiciones de los atributos de vehiculo */
+                @CheckConstraint(name = "chk_tipo_vehiculo", constraint = "tipo_vehiculo IN ('AUTOMOVIL', 'MOTO')"),
+                @CheckConstraint(name = "chk_placa", constraint = "(tipo_vehiculo = 'AUTOMOVIL' AND placa REGEXP '^[A-Za-z]{3}[0-9]{3}$')"
+                                +
+                                "OR (tipo_vehiculo = 'MOTO' AND placa REGEXP '^[A-Za-z]{3}[0-9]{2}[A-Za-z]$'))"),
+                @CheckConstraint(name = "chk_cap_pasajeros", constraint = "(capacidad_pasajeros >= 0)"),
+                @CheckConstraint(name = "chk_modelo", constraint = "(modelo >= 0)")
 })
 public class VehiculoModel {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "tipo_vehiculo", nullable = false)
-    private TipoVehiculo tipoVehiculo;
+        @Enumerated(EnumType.STRING)
+        @Column(name = "tipo_vehiculo", nullable = false)
+        private TipoVehiculo tipoVehiculo;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "tipo_servicio", nullable = false)
-    private TipoServicio tipoServicio;
+        @Enumerated(EnumType.STRING)
+        @Column(name = "tipo_servicio", nullable = false)
+        private TipoServicio tipoServicio;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "tipo_combustible", nullable = false)
-    private TipoCombustible tipoCombustible;
+        @Enumerated(EnumType.STRING)
+        @Column(name = "tipo_combustible", nullable = false)
+        private TipoCombustible tipoCombustible;
 
-    @Column(name = "placa", nullable = false, unique = true, length = 6)
-    private String placa;
+        @Column(name = "placa", nullable = false, unique = true, length = 6)
+        private String placa;
 
-    @Column(name = "capacidad_pasajeros", nullable = false)
-    private Integer capacidadPasajeros;
+        @Column(name = "capacidad_pasajeros", nullable = false)
+        private Integer capacidadPasajeros;
 
-    @Column(name = "color", nullable = false)
-    private String colorHexadecimal;
+        @Column(name = "color", nullable = false)
+        private String colorHexadecimal;
 
-    @Column(name = "medelo", nullable = false)
-    private Integer modelo;
+        @Column(name = "medelo", nullable = false)
+        private Integer modelo;
 
-    @Column(name = "marca", nullable = false)
-    private String marca;
+        @Column(name = "marca", nullable = false)
+        private String marca;
 
-    @Column(name = "linea", nullable = false)
-    private String linea;
+        @Column(name = "linea", nullable = false)
+        private String linea;
 }
