ec4f3d81e706690ea7b55831a723c9b0e7dd4a5e
RojasJhonatan <jmr.85@hotmail.com>
2026-09-23 12:00:49 -0500

    Creación PersonaModel.java

---
+++ src/main/java/com/example/tutoria1/Enums/Persona/TipoDocumento.java
@@ -0,0 +1,7 @@
+package com.example.tutoria1.Enums.Persona;
+
+public enum TipoDocumento {
+    CC, // Cédula Coudadanía
+    CE, // Cédula de Extranjería
+    PEP // Permiso Especial de Permanencia
+}
